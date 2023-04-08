a348f057b635a1791e632a55f4e9ec45332d0969
yusuf <kingshakzy@gmail.com>
2023-04-08 17:55:29 +0100

    Basic features up and running

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-var viewModel = function (){
+let viewModel = function (){
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Tabby");
     this.imgSrc = ko.observable("img/tabbyCat.jpg");
+    // let v = 0
     this.incrementCount = function(){
-        this.clickCount(this.clickCount + 1)
+        this.clickCount(this.clickCount() + 1);
+        // console.log(this.clickCount());
     }
 }
 ko.applyBindings(new viewModel())
