4e5e6b851090bdd1f037ed978e38f211a91edd41
yusuf <kingshakzy@gmail.com>
2023-04-08 18:19:30 +0100

    Updated features up

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
-let viewModel = function (){
+let Cat = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Tabby");
     this.imgSrc = ko.observable("img/tabbyCat.jpg");
-    this.level = ko.observable("Infant")
+    this.level = ko.observable("Infant");
+    this.nickNames = ko.observableArray([
+        "Tabby", "Mick", "Taamara", "Lincoln"
+    ])
+}
+
+let viewModel = function (){
+   this.currentCat = ko.observable(new Cat())
     // let v = 0
     this.incrementCount = function(){
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
         // console.log(this.clickCount());
-        if(this.clickCount() > 10){
+        if(this.currentCat().clickCount() > 10){
             this.level("Teen");
-        }if(this.clickCount() > 20){
+        }if(this.currentCat().clickCount() > 20){
             this.level("Older Teen")
         }
     }
-}
+};
 ko.applyBindings(new viewModel())
